f68d6fb9a3533c0f1d040c87afabe90cf66b84e6
Tolulope Olagunju <65245607+Powerisinschool@users.noreply.github.com>
2022-06-20 11:17:34 +0100

    cleanup

---
+++ README.md
@@ -0,0 +1,11 @@
+# Image Converter with go
+
+## Use:
+<code>
+image-converter [format1] [format2]
+</code>
+
+## Example Use:
+Assuming a file is existent in the current directory called image.jpg:
+<code>
+image-converter image.jpg
--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	// "fmt"
 	"image-converter/converter"
-	// "image-converter/match"
 	_ "net/http"
 	"os"
 )
@@ -22,11 +20,6 @@ func main() {
 		if secondArg == "x" {
 			secondArg = ""
 		}
-		// if match.IsMatch(args[1], "*.png") {
-		// 	Convert()
-		// } else {
-		// 	help()
-		// }
 
 		err := converter.Convert(args[1], secondArg)
 
@@ -38,28 +31,6 @@ func main() {
 	if len(args) == 2 {
 		panic("Coming soon...\nInput at least 1 argument")
 	}
-
-	// You can register another format here
-	// image.RegisterFormat("png", "png", png.Decode, png.DecodeConfig)
-	// image.RegisterFormat("jpeg", "jpeg", jpeg.Decode, jpeg.DecodeConfig)
-
-	// file, err := os.Open("./image.png")
-
-	// if err != nil {
-	// 	fmt.Println("Error: File could not be opened")
-	// 	os.Exit(1)
-	// }
-
-	// defer file.Close()
-
-	// fmt.Println(converter.PNGToJPEG("./image.png", "image.jpeg"))
-	// fmt.Println(converter.PNGToJPEG("./image.png", "image.gif"))
-	// fmt.Println(converter.PNGToWEBP("./image.png", "image.webp"))
-
-	// if err != nil {
-	// 	fmt.Println("Error: Image could not be decoded")
-	// 	os.Exit(1)
-	// }
 }
 
 func help(err error) {
